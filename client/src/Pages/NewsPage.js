fd9095ec7500712a1f3026394ab544dd01235072
fabiogrillo <fabiogrillo123@gmail.com>
2024-03-12 19:13:42 +0100

    added text editor

--- client/src/Pages/NewsPage.js
+++ client/src/Pages/NewsPage.js
@@ -1,11 +1,57 @@
-import { Button, Container, ListGroup, ListGroupItem } from "react-bootstrap"
+import { ListGroup, ListGroupItem, Container } from "react-bootstrap"
+import ReactQuill, { Quill } from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+    toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        [{ 'font': [] }],
+        ['blockquote', 'code-block'],
+        ['link', 'image'],
+
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+        [{ 'align': [] }],
+        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+
+        ['clean']
+    ]
+}
+
+const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+]
 
 function NewsPage() {
     return (
-        <ListGroup variant="flush" style={{borderRadius:'15px'}}>
-            <ListGroup.Item >el 1</ListGroup.Item>
-            <ListGroup.Item>el 2</ListGroup.Item>
-        </ListGroup>
+        <>
+            <ListGroup variant="flush" style={{ borderRadius: '15px' }}>
+                <ListGroup.Item >el 1</ListGroup.Item>
+                <ListGroup.Item>el 2</ListGroup.Item>
+            </ListGroup>
+            <Container style={{ borderRadius:'15px'}}>
+                <ReactQuill theme="snow" modules={modules} style={{ marginTop: '3em', backgroundColor:'white' }} 
+                    formats={formats} placeholder="Insert text here..." />
+            </Container>
+        </>
     )
 }
 
